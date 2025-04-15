7aff0018eca2f848e43b4d740c5e345a277d77f7
Ruslan Moiseyenko <ruslan@atomix-chat.com>
2025-04-15 10:54:57 +0300

    add Helmet, Passport, ValidationPipe, ThrottlerModule, CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,6 +10,12 @@ import { AppService } from './app.service';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // 1 minute
+        limit: 100, // max 100 requests per minute
+      },
+    ]),
     PrismaModule,
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? process.env.ALLOWED_ORIGINS?.split(',')
+        : true,
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Removes non-whitelisted properties
+      forbidNonWhitelisted: true, // Throws errors if non-whitelisted values are provided
+      transform: true, // Auto-transform payloads to DTO instances
+    }),
+  );
+
+  app.use(
+    helmet({
+      contentSecurityPolicy:
+        process.env.NODE_ENV === 'production' ? undefined : false,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
