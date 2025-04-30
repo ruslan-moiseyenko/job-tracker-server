baf8b94239ab7ff0aa83aa8f7b8cb1c99dfb3816
Ruslan Moiseyenko <ruscom5@gmail.com>
2025-04-30 12:15:51 +0300

    Add BadUserInputError

--- src/common/exceptions/graphql.exceptions.ts
+++ src/common/exceptions/graphql.exceptions.ts
@@ -31,6 +31,13 @@ export class NotFoundError extends GraphQLError {
     });
   }
 }
+export class BadUserInputError extends GraphQLError {
+  constructor(message: string) {
+    super(message, {
+      extensions: { code: 'BAD_USER_INPUT' },
+    });
+  }
+}
 
 export class OAuthError extends GraphQLError {
   constructor(message: string, details?: Record<string, any>) {
